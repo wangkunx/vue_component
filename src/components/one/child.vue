4075f14f3a4c1c88c47464e2bcd0f69622d79e3f
wangkunmo <1510857471@qq.com>
2019-01-24 16:51:41 +0800

    change in master111

--- src/components/one/child.vue
+++ src/components/one/child.vue
@@ -49,7 +49,7 @@ export default{
     init(){
 //    I was changed in dev and master branch
       console.log(this.$el.getAttribute('data'))
-//    change in master and dev
+//    change in master
     },
   },
   inheritAttrs: false,
